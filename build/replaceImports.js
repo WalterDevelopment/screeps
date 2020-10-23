4a4f6aa85775714ca43c319a60cc9ccd9c974242
electricdanwa <electricdanwa@gmail.com>
2020-10-22 20:30:43 -0400

    Added npm task to replace slashes with underscores in require statements

---
+++ build/replaceImports.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const nodeDir = require('node-dir');
+
+nodeDir.readFiles(
+  './dist', // the root path
+
+  // an options object
+  {
+    match: /.js$/, // only match json files
+    recursive: false, // only the root dir
+  },
+
+  function (err, content, filename, next) {
+    if (err) {
+      console.log(err);
+    } else {
+      const regex = /(require\(')(.*)('\))/g;
+
+      function replacer(match, p1, p2, p3, offset, string) {
+        return p1 + p2.replace(/\.\//g, '').replace(/\//g, '_') + p3;
+      }
+
+      fs.readFile(filename, 'utf8', function (err, data) {
+        if (err) return console.log(err);
+
+        var result = data.replace(regex, replacer);
+        fs.writeFile(filename, result, 'utf8', function (err) {
+          if (err) return console.log(err);
+        });
+      });
+
+      next();
+    }
+  }
+);
--- src/config/creepSettings.js
+++ src/config/creepSettings.js
@@ -1,39 +1,39 @@
-const roleBuilder = require('roles_builder');
-const roleHarvester = require('roles_harvester');
-const roleUpgrader = require('roles_upgrader');
+const roleBuilder = require('roles/builder');
+const roleHarvester = require('roles/harvester');
+const roleUpgrader = require('roles/upgrader');
 
 module.exports = [
-    {
-        count: 2,
-        job: roleBuilder.run,
-        role: 'builder',
-        generations: [
-            {
-                generation: 1,
-                body: [WORK, CARRY, MOVE]
-            }
-        ]
-    },
-    {
-        count: 2,
-        job: roleHarvester.run,
-        role: 'gatherer',
-        generations: [
-            {
-                generation: 1,
-                body: [WORK, CARRY, MOVE]
-            }
-        ]
-    },
-    {
-        count: 1,
-        job: roleUpgrader.run,
-        role: 'upgrader',
-        generations: [
-            {
-                generation: 1,
-                body: [WORK, CARRY, MOVE]
-            }
-        ]
-    }
+  {
+    count: 2,
+    job: roleBuilder.run,
+    role: 'builder',
+    generations: [
+      {
+        generation: 1,
+        body: [WORK, CARRY, MOVE],
+      },
+    ],
+  },
+  {
+    count: 2,
+    job: roleHarvester.run,
+    role: 'gatherer',
+    generations: [
+      {
+        generation: 1,
+        body: [WORK, CARRY, MOVE],
+      },
+    ],
+  },
+  {
+    count: 1,
+    job: roleUpgrader.run,
+    role: 'upgrader',
+    generations: [
+      {
+        generation: 1,
+        body: [WORK, CARRY, MOVE],
+      },
+    ],
+  },
 ];
