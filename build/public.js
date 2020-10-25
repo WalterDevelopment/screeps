a2c2d3c1b18508bfb1d1f4be35460de04a98cf2a
William Walter <wilwa74@gmail.com>
2020-10-25 00:10:48 -0400

    fixed error with logging

--- build/public.js
+++ build/public.js
@@ -12,62 +12,62 @@ const screepsBranch = env.branch ? env.branch : config.branch;
 let modules = {};
 
 function base64_encode(file) {
-  // read binary data
-  var fileContents = fs.readFileSync(file);
-  // convert binary data to base64 encoded string
-  return Buffer.from(fileContents).toString('base64');
+    // read binary data
+    var fileContents = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return Buffer.from(fileContents).toString('base64');
 }
 
 nodeDir.readFiles(
-  './dist', // the root path
+    './dist', // the root path
 
-  // an options object
-  {
-    match: /.js$/, // only match JavaScript files
-    recursive: false, // only the root dir
-  },
+    // an options object
+    {
+        match: /.js$/, // only match JavaScript files
+        recursive: false // only the root dir
+    },
 
-  function (err, content, filename, next) {
-    if (err) {
-      console.log(err);
-    } else {
-      var basename = path.basename(filename),
-        ext = path.extname(basename),
-        name = basename.replace(ext, '');
+    function (err, content, filename, next) {
+        if (err) {
+            console.log(err);
+        } else {
+            var basename = path.basename(filename),
+                ext = path.extname(basename),
+                name = basename.replace(ext, '');
 
-      if (ext === '.js') {
-        modules[name] = content;
-      } else {
-        modules[name] = base64_encode(filename);
-      }
+            if (ext === '.js') {
+                modules[name] = content;
+            } else {
+                modules[name] = base64_encode(filename);
+            }
 
-      console.log(`${basename}, ${filename}, ${ext}, ${name}`);
+            console.log(`${basename}, ${filename}, ${ext}, ${name}`);
 
-      next();
+            next();
+        }
+    },
+    function () {
+        var email = screepsEmail,
+            password = screepsPassword,
+            data = {
+                branch: screepsBranch,
+                modules: modules
+            };
+
+        console.log(`branch: ${screepsBranch}`);
+
+        var req = https.request({
+            hostname: 'screeps.com',
+            port: 443,
+            path: '/api/user/code',
+            method: 'POST',
+            auth: email + ':' + password,
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        });
+
+        req.write(JSON.stringify(data));
+        req.end();
     }
-  },
-  function () {
-    var email = screepsEmail,
-      password = screepsPassword,
-      data = {
-        branch: screepsBranch,
-        modules: modules,
-      };
-
-    console.log(`branch: ${branch}`);
-
-    var req = https.request({
-      hostname: 'screeps.com',
-      port: 443,
-      path: '/api/user/code',
-      method: 'POST',
-      auth: email + ':' + password,
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-      },
-    });
-
-    req.write(JSON.stringify(data));
-    req.end();
-  }
 );
